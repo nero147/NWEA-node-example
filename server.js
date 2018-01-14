a0037e9783664d903cf36c6cac17ea11497654c5
Noah Gildersleeve <noah.gildersleeve@fovia.com>
2018-01-14 14:30:32 -0800

    Added new dependencies to package.json and implemented post command to go to database and autoincrement primary key, but currently fails if database already exists

--- server.js
+++ server.js
@@ -1,16 +1,35 @@
-'use strict';
+// grab the packages we need
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+var timeout = require('connect-timeout')
+app.use(bodyParser.json()); // support json encoded bodies
+app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
+app.use(timeout('3s')); //kills the session with a timeout
+var port = process.env.PORT || 8080;
 
-const express = require('express');
+// DB stuff here
+var sqlite3 = require('sqlite3').verbose();
+var db = new sqlite3.Database('db.sqlite');
+db.run("CREATE TABLE blogs (id integer primary key autoincrement, posts TEXT);");
 
-// Constants
-const PORT = 8080;
-const HOST = '0.0.0.0';
+// routes will go here
+app.get('/blog/get', function(req, res) {
+  var user_id = req.param('id');
+  var token = req.param('token');
+  var geo = req.param('geo');  
 
-// App
-const app = express();
-app.get('/', (req, res) => {
-  res.send('Hello world\n');
+  res.send(user_id + ' ' + token + ' ' + geo);
+});
+// POST http://localhost:8080/blog/post
+// parameters sent with 
+app.post('/blog/post', function(req, res) {
+    var post = req.body.post;
+    var stmt = db.prepare("INSERT INTO blogs(posts) VALUES(?);", post);
+    stmt.run();
+    stmt.finalize();
 });
 
-app.listen(PORT, HOST);
-console.log(`Running on http://${HOST}:${PORT}`);
+// start the server
+app.listen(port);
+console.log('Server started! At http://localhost:' + port);
